53b198c903fcdd19310c0b761d179061729b89ff
biro.vince <quick.biro@gmail.com>
2020-04-15 14:02:56 +0200

    search for movies

--- src/ui/content/movies.tsx
+++ src/ui/content/movies.tsx
@@ -14,40 +14,44 @@ interface Movie {
     title: string
 }
 
+interface lmProps {
+  searchString: string
+}
+
+function ListMovies(props: lmProps) {
+  const search = props.searchString
+  
+  const { loading:isLoading, error, data } = useQuery(MOV_Q, { variables: { search: search } })
+    
+  if (isLoading) return <p>Loading ...</p>
+  if (error) return <h1>Log in to view movies.</h1>
+
+  if (data.movies.length === 0) return <h1>No movies found.</h1>
+  const movies = data.movies
+  const listItems = movies.map((movie: Movie, index: number) =>
+        <h1 key={index}>{movie.title}</h1>
+  )
+  return listItems
+}
+
 //is it good to double check whether user is logged in? eg: in greeting, nav, content
 export function Movies() {
-    const [search, upSearch] = useState('')
-    const { loading, error, data } = useQuery(MOV_Q)
-    /*const [loadGreeting, { called, loading2, data2 }] = useLazyQuery(
-      MOV_Q,
-      { variables: { search: search } }
-    )*/
-    if (loading) return <p>Loading ...</p>
-    if(error) console.log(error)
-    if (error) return <h1>Log in to view movies.</h1>
-    if (data.movies.length === 0) return <h1>No movies found.</h1>
-    const movies = data.movies
-    console.log(movies)
-    const listItems = movies.map((movie: Movie, index: number) =>
-        <h1 key={index}>{movie.title}</h1>
-    )
-    
+    const [search, upSearch] = useState('')    
 
     function handleSubmit(event: any) {
-      if(search) {
-        
-      }
+      event.preventDefault()
     }
 
     function handleChange(event: any) {
-      upSearch(event.target.name)
+      upSearch(event.target.value)
     }
 
     return (
         <div>
           <form onSubmit={handleSubmit}>
             <input type="text" name="search" onChange={handleChange} value={search} placeholder="type to search" />
+            <button onClick={()=>upSearch('')}>Reset</button>
           </form>
-          {listItems}
+          <ListMovies searchString={search}/>
         </div>)
 }
